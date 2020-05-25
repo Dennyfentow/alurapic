f1fe93ea784d911310a6cc636d2b0dd02cc6643e
Daniel Bentes <megabradock@gmail.com>
2020-05-25 01:32:48 -0300

    parte3-1 Upload de Photos Funcionando

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -24,4 +24,12 @@ export class PhotoService {
             .get<Photo[]>(`${API}/${username}/photos`, { params });
     }
 
+    upload(description: string, allowComments: boolean, file: File) {
+        const formData = new FormData();
+        formData.append('description', description);
+        formData.append('allowComments', allowComments ? 'true': 'false');
+        formData.append('imageFile', file);
+
+        return this.http.post(API + '/photos/upload', formData);
+    }
 }
